c3dbe3025505d9493d3a7d8ac586382a5dd69bb9
wojciechfpga <lasowski.wojciech.poczta@gmail.com>
2024-12-07 22:06:47 +0100

    Commands Rooms - blocking doubling

--- backend/app/commands/rooms_commands.py
+++ backend/app/commands/rooms_commands.py
@@ -6,13 +6,26 @@ class CreateRoomCommand:
     @staticmethod
     def execute(name, capacity):
         """
-        Create a new room and save to the database.
+        Create a new room and save to the database, ensuring no room with the same name exists.
         """
         try:
+            existing_room = Room.query.filter_by(name=name).first()
+            if existing_room:
+                raise ValueError(f"A room with this name already exists.")
+
             room = Room(name=name, capacity=capacity)
             db.session.add(room)
             db.session.commit()
             return room.id
+        
+        except ValueError as ve:
+            current_app.logger.info(ve)
+            abort(409, description=str(ve))
+        
         except Exception as e:
             current_app.logger.error(f"Error creating room: {e}")
-            abort(500, description="Error creating room")
+            abort(500, description="Error creating room") 
+        
+        finally:
+            print("Create room operation attempt logged")
+
