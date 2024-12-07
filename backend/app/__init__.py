e76877473d132e94cbc72363ae0f92b6985cdf76
wojciechfpga <lasowski.wojciech.poczta@gmail.com>
2024-12-07 12:09:43 +0100

    Swagger

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
+from flask_swagger_ui import get_swaggerui_blueprint
 from config import Config
 
 db = SQLAlchemy()
@@ -14,10 +15,23 @@ def create_app():
     db.init_app(app)
 
     # Rejestracja blueprintów
-    from app.routes import bp as room_blueprint,bp_reservation,bp_auth
+    from app.routes import bp as room_blueprint, bp_reservation, bp_auth
     app.register_blueprint(room_blueprint, url_prefix='/api')
     app.register_blueprint(bp_reservation, url_prefix="/api")
     app.register_blueprint(bp_auth, url_prefix="/api")
+
+    # Swagger UI
+    SWAGGER_URL = '/swagger'
+    API_URL = '/static/swagger.json'  # Ścieżka do pliku JSON z opisem API
+    swaggerui_blueprint = get_swaggerui_blueprint(
+        SWAGGER_URL,
+        API_URL,
+        config={
+            'app_name': "Conference Room Booking API"
+        }
+    )
+    app.register_blueprint(swaggerui_blueprint, url_prefix=SWAGGER_URL)
+
     # Komenda CLI do inicjalizacji bazy danych
     @app.cli.command("init-db")
     def init_db():
